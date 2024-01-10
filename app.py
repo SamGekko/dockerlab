d396e7cbec623df12b18ec8b256fbc294ec46ed7
SamGekko <alex2002kuza@gmail.com>
2024-01-10 03:34:54 +0300

    Commit_Dev_1

---
+++ app.py
@@ -0,0 +1,22 @@
+import os
+
+def char_to_int(char_variable):
+    try:
+        result = int(char_variable)
+        return result
+    except ValueError:
+        return None  # Возвращаем None в случае ошибки
+
+if __name__ == "__main__":
+    # Чтение символа из переменной окружения
+    char_variable = os.getenv("CHAR_VARIABLE")
+
+    if char_variable is None:
+        print("Переменная окружения CHAR_VARIABLE не определена.")
+    else:
+        result = char_to_int(char_variable)
+        if result is not None:
+            print(result)
+        else:
+            print("Ошибка: Введенный символ не является цифрой.")
+
